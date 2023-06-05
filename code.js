074972868eac0571298c5164e8e5b6522cb5f2a6
victor calderon <calderon.j.victor@gmail.com>
2023-06-05 12:12:54 -0500

    05/06

--- code.js
+++ code.js
@@ -1,4 +1,19 @@
-// helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// helloWorld function
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+// function sayHello(input) {
+//
+//     if (input === "alex") {
+//         return "Hello, Alex!";
+//     } else if (input === "pat") {
+//         return "Hello, Pat!"
+//     } else {
+//         return "Hello, Jane!";
+//     }
+// }
+
+function sayHello(input) {
+    return "Hello, " + input + "!";
 }
